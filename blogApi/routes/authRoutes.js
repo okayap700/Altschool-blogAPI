1817450dc6beaa2e133c1c79e6797e1baf4c6d37
Okay Chukwu <generalchukzzy.co@gmail.com>
2024-05-12 21:43:59 +0100

    Update authRoutes.js

--- blogApi/routes/authRoutes.js
+++ blogApi/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authContoller.js');
+const authController = require('../controllers/authContoller');
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
